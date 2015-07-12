c8e084f13be2c7e31b17db366b6536559ba21076
Sebastian Schäfer <sarek@uliweb.de>
2015-07-12 19:14:25 +0200

    Main program to perform checks

--- src/de/l8systems/check_java/Main.java
+++ src/de/l8systems/check_java/Main.java
@@ -30,9 +30,118 @@
 
 package de.l8systems.check_java;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        try {
+            Options opts = new Options(args);
+
+            List<AttributeIdentifier> attrs = new LinkedList<AttributeIdentifier>();
+            String hdr = "UNKNOWN";
+            switch (opts.getCheckType()) {
+                case HeapMem:
+                    attrs.add(new AttributeIdentifier("java.lang:type=Memory", "HeapMemoryUsage", "used", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=Memory", "HeapMemoryUsage", "init", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=Memory", "HeapMemoryUsage", "committed", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=Memory", "HeapMemoryUsage", "max", "B"));
+                    hdr = "Heap Memory Usage";
+                    break;
+
+                case NonHeapMem:
+                    attrs.add(new AttributeIdentifier("java.lang:type=Memory", "NonHeapMemoryUsage", "used", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=Memory", "NonHeapMemoryUsage", "init", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=Memory", "NonHeapMemoryUsage", "committed", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=Memory", "NonHeapMemoryUsage", "max", "B"));
+                    hdr = "Non-Heap Memory Usage";
+                    break;
+
+                case MemPoolEden:
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=PS Eden Space", "Usage", "used", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=PS Eden Space", "Usage", "init", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=PS Eden Space", "Usage", "committed", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=PS Eden Space", "Usage", "max", "B"));
+                    hdr = "Memory Pool PS Eden Space Usage";
+                    break;
+
+                case MemPoolSurvivor:
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=PS Survivor Space", "Usage", "used", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=PS Survivor Space", "Usage", "init", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=PS Survivor Space", "Usage", "committed", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=PS Survivor Space", "Usage", "max", "B"));
+                    hdr = "Memory Pool PS Survivor Space Usage";
+                    break;
+
+                case MemPoolCodeCache:
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=Code Cache", "Usage", "used", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=Code Cache", "Usage", "init", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=Code Cache", "Usage", "committed", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=Code Cache", "Usage", "max", "B"));
+                    hdr = "Memory Pool Code Cache Usage";
+                    break;
+
+                case MemPoolOldGen:
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=PS Old Gen", "Usage", "used", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=PS Old Gen", "Usage", "init", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=PS Old Gen", "Usage", "committed", "B"));
+                    attrs.add(new AttributeIdentifier("java.lang:type=MemoryPool,name=PS Old Gen", "Usage", "max", "B"));
+                    hdr = "Memory Pool PS Old Gen Usage";
+                    break;
+
+                case GCMarkSweepCount:
+                    attrs.add(new AttributeIdentifier("java.lang:type=GarbageCollector,name=PS MarkSweep", "CollectionCount", ""));
+                    hdr = "Garbage Collection Count";
+                    break;
+
+                case GCMarkSweepTime:
+                    attrs.add(new AttributeIdentifier("java.lang:type=GarbageCollector,name=PS MarkSweep", "CollectionTime", "ms"));
+                    hdr = "Gargabe Collection Time";
+                    break;
+
+                case ClassLoading:
+                    attrs.add(new AttributeIdentifier("java.lang:type=ClassLoading", "LoadedClassCount", ""));
+                    attrs.add(new AttributeIdentifier("java.lang:type=ClassLoading", "TotalLoadedClassCount", ""));
+                    attrs.add(new AttributeIdentifier("java.lang:type=ClassLoading", "UnloadedClassCount", ""));
+                    hdr = "Loaded Classes";
+                    break;
+
+                case ThreadCount:
+                    attrs.add(new AttributeIdentifier("java.lang:type=Threading", "ThreadCount", ""));
+                    attrs.add(new AttributeIdentifier("java.lang:type=Threading", "TotalStartedThreadCount", ""));
+                    attrs.add(new AttributeIdentifier("java.lang:type=Threading", "DaemonThreadCount", ""));
+                    hdr = "Threads";
+                    break;
+            }
+
+            List<Result> res = new LinkedList<Result>();
+            JMXClient client = new JMXClient(opts.getUrl());
+
+            for (AttributeIdentifier attr : attrs) {
+                res.add(attr.getValue(client));
+            }
+
+            String status = "OK";
+            int statval = 0;
+            if (res.get(0).getValue() > opts.getWarnThreshold()) {
+                status = "WARN";
+                statval = 1;
+
+                if (res.get(0).getValue() > opts.getCritThreshold()) {
+                    status = "CRITICAL";
+                    statval = 2;
+                }
+            }
+
+            System.out.println(status + ": " + hdr + ": " + res.get(0).getValue() + " " + res.get(0).getUnit() + "/" + opts.getWarnThreshold() + "/" + opts.getCritThreshold());
+            for (Result r : res) {
+                System.out.println("'" + r.getName() + "'=" + r.getValue() + r.getUnit());
+            }
+            System.exit(statval);
+        } catch (Exception e) {
+            System.out.println("Could not retrieve data: " + e.getClass().getName() + ": " + e.getMessage());
+            System.exit(3);
+        }
     }
 }
